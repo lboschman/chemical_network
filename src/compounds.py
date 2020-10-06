c1b25160a18546d754562d978d4a797401e30d99
Leon Boschman <leon.boschman@gmail.com>
2020-10-06 21:20:31 +0200

    Added some basic documentation to the compounds.py

--- src/compounds.py
+++ src/compounds.py
@@ -2,18 +2,23 @@
 
 
 class Compound:
+    """A chemical compound in the network, as a name and a number density
+
+    """
     def __init__(self, name: str, density=0.):
         self._name = name
         self._density = density
 
     @property
     def name(self):
+        """The name of the compound"""
         return self._name
 
     @property
     def density(self):
+        """The number density of the compound"""
         return self._density
 
     @density.setter
-    def density(self, density):
+    def density(self, density: float):
         self._density = density
