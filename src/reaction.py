0ce67816f628e1cbbb082c3a6045563b43bbab02
Leon Boschman <leon.boschman@gmail.com>
2020-10-06 21:15:48 +0200

    Cleaned up reaction.py

--- src/reaction.py
+++ src/reaction.py
@@ -3,15 +3,16 @@
 import compounds
 
 import numpy as np
-import scipy as sc
 from scipy.constants import Boltzmann
-
 from typing import List
 
 
 class Reaction:
+    """A reaction that follows Arrhenius kinetics (https://en.wikipedia.org/wiki/Arrhenius_equation).
+
+    """
     def __init__(self, reactants: List[compounds.Compound], products: List[compounds.Compound],
-                 sigma: float, barrier: float, norm: float=1):
+                 sigma: float, barrier: float, norm: float = 1):
         """Initiate the reaction.
 
         :param reactants: reactants in the reaction
@@ -26,12 +27,13 @@ class Reaction:
         self.barrier = barrier
         self.norm = norm
 
-    def get_rate(self, t_gas=300):
+    def get_rate(self, t_gas: float = 300.) -> float:
+        """Calculate the reaction rate.
+
+        :param t_gas: the gas temperature for the Arrhenius equation
+        :return: the reaction rate
+        """
         concentration = 1
         for reactant in self.reactants:
             concentration *= reactant.density
         return self.norm * concentration * self.sigma * np.exp(- self.barrier/(Boltzmann * t_gas))
-
-
-
-
