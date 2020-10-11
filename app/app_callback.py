401430ee1b3005d24abdc34c76b47d2ab7147638
Leon Boschman <leon.boschman@gmail.com>
2020-10-11 16:18:42 +0200

    Removed unused imports

--- app/app_callback.py
+++ app/app_callback.py
@@ -1,13 +1,9 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
 
 import plotly.graph_objs as go
-import base64
 import pandas as pd
-import io
 
 import chem_network as cn
 
